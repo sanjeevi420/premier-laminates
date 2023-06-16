500eea46c8cf7808c28a6a6e367a97c61580f7c5
sanjeevi420 <sgsanjeevi.gopal@gmail.com>
2023-06-16 15:50:40 +0530

    removing test code

--- laminator/laminator.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-laminator',
-  templateUrl: './laminator.component.html',
-  styleUrls: ['./laminator.component.css']
-})
-export class LaminatorComponent {
-}
